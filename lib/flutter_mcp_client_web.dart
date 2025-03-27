4e90155a320ebe4322c870bd3744215a6988a89c
jsha21 <jsha@ais21.co.kr>
2025-03-27 12:58:39 +0900

    first commit

---
+++ lib/flutter_mcp_client_web.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// Web implementation of the flutter_mcp_client plugin.
+class FlutterMcpClientWeb {
+  static void registerWith(Registrar registrar) {
+  }
+}
